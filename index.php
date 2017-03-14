a8bd015816fdcd1dc4e36d9c5d338bfda408a8c6
Justice aluya <justice015@gmail.com>
2017-03-14 15:33:36 +0000

    db init

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
  * Date: 07/03/2017
  * Time: 16:49
  */
+// create a SQL query as a string
+$sql_query = "SELECT * FROM marvelmovies ";
+// execute the SQL query
+//$result = $db->query($sql_query);
+$db="localdb";
 
 $result=mysqli_query($db,$sql);
 while($row = $result->fetch_array())
