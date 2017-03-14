44a1eae790bf3d4010d1ff5036b43aca4444b9c7
Justice aluya <justice015@gmail.com>
2017-03-14 16:24:58 +0000

    db init

--- index.php
+++ index.php
@@ -10,13 +10,13 @@ include "dbConnect.php";
 $sql = "SELECT * FROM marvelmovies ";
 // execute the SQL query
 //$result = $db->query($sql);
-$db="localdb";
+//$db="localdb";
 $result=mysqli_query($link,$sql);
 while($row = $result->fetch_array())
 {
     /* the code inside here is repeated for each item in the array
     You can do things like the following to print out each movie title */
-
+    echo "row 1 <br>";
     $movieTitle = $row['title'];
     echo "<p>" . $movieTitle . "</p>";
 
