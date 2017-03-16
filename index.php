e1b98f99cbd90449c1b16a2b5f1e113737d436fa
just-cmm503 <cmm503-Just>
2017-03-16 14:41:09 +0000

    new

--- index.php
+++ index.php
@@ -11,15 +11,13 @@ $sql = "select * from marvelmovies";
 // execute the SQL query
 //$result=mysqli_query($link,$sql);
 $result=$link->query($sql);
-echo mysqli_num_rows($result)." ... ... ok ... ... ";
+//echo mysqli_num_rows($result)." rows returned from database ";
 while($row = $result->fetch_array())
 {
     /* the code inside here is repeated for each item in the array
     You can do things like the following to print out each movie title */
-    echo "row 1 <br>";
     $movieTitle = $row['title'];
     echo "<p>" . $movieTitle . "</p>";
-
 }
 $result->close();
 $link->close();
