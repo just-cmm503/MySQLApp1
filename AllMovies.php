6bcc8690e6bd491a1141eea3015cb23eb8b8be0f
just-cmm503 <cmm503-Just>
2017-03-16 21:21:02 +0000

    new

--- AllMovies.php
+++ AllMovies.php
@@ -10,20 +10,22 @@ $sql = "select * from marvelmovies";
 // execute the SQL query
 //$result=mysqli_query($link,$sql);
 $result=$link->query($sql);
-//echo mysqli_num_rows($result)." rows returned from database ";
+
 while($row = $result->fetch_array())
 {
     /* the code inside here is repeated for each item in the array
     You can do things like the following to print out each movie title */
     print " <div class='row'> \r\n";
-    //$movieTitle = $row['title'];
-    //print "<p>" . $movieTitle . "</p>";
-
-    foreach ($row as &$col){
-        print " <div class='col'> \r\n";
-        print "  $col  ";
-        print " </div> \r\n";
+    $count1=0;
+    foreach ($row as $col){
+        $count1++;
+        if (mod($count1,2) ==0) {
+            print " <div class='col'> \r\n";
+            print "  $col  ";
+            print " </div> \r\n";
+        }
     }
+
     print " </div> \r\n";
 }
 $result->close();
